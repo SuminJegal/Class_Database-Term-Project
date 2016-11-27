ee8c17c6e37d1be1c279c84c6e8c2e9dfb2cb0f9
SuminJegal <geniusry@naver.com>
2016-11-28 01:27:31 +0900

    modify for eclipse

---
+++ MovieBookingSystem1/src/Customer.java
@@ -0,0 +1,27 @@
+public class Customer {
+    
+    public void login(){
+        
+    }
+    
+    public void signUp(){
+        
+    }
+    
+    public void bookingMovie(){
+        
+    }
+    
+    public void SearchMovie(){
+        
+    }
+    
+    public void modifyMyInformation(){
+        
+    }
+    
+    public void secession(){
+        
+    }
+
+}
